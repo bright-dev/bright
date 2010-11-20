77fa40c7c04b745de2934b998a3c195fa3e3eb80
Anthony Scopatz <scopatz@gmail.com>
2010-11-20 03:33:30 -0600

    Added zero arrays to beginning of file.

--- char/m2py.py
+++ char/m2py.py
@@ -11,10 +11,10 @@ if_idx_str = ("""if (exist("idx", "var"));\n"""
 
 num_pattern = "([0-9]+[.]?[0-9]*[Ee]?[+-]?[0-9]*)"
 
-array_pattern = r"\[[0-9\sEe+-.]*\]"
+numpy_array_pattern = r"\[[0-9\sEe+-.,%]*\]"
+matlab_array_pattern = r"\[[0-9\sEe+-.%]*\]"
 
-def num_replace(matchobj):
-    return matchobj.group(0) + ','
+lhs_variable_pattern = r"(\w+)\s*(\(idx.*?\))"
 
 def convert(filename):
     """Convert a matlab *.m file to a python file."""
@@ -32,9 +32,41 @@ def convert(filename):
     f = fpart[0] + "idx = 0" + fpart[2]
     f = f.replace(if_idx_str, 'idx += 1')
 
-    arrays = re.findall(array_pattern, f)
+    # Replace matlab arrays with python lists
+    arrays = re.findall(matlab_array_pattern, f)
     for a in arrays:
-        new_a = re.sub(num_pattern, num_replace, a)
+        new_a = re.sub(num_pattern, lambda mo: mo.group(0) + ',', a)
         f = f.replace(a, new_a)
+
+    # Encapsulate python lists in numpy arrays
+    f = re.sub(numpy_array_pattern, lambda mo: 'np.array(' + mo.group(0) + ')', f)
+
+
+    # Add imports to header
+    header = "import numpy as np\n\n"
+
+    # Find all variables and shape
+    vars_shape = np.unique( re.findall(lhs_variable_pattern, f) )
+    # Initialize variables to zero
+    header = header + "# Initialize variables\n"
+    for vs in vars_shape:
+        s = re.search(r'\[.*:(.*?)\]', vs[1])
+        if s == None:
+            vs_shape = ""
+        else:
+            vs_shape = s.group(1)
+            vs_shape = vs_shape.split()
+            vs_shape = ", ".join(vs_shape)
+
+        line = "{0} = np.zeros([{1}, {2}])\n".format(vs[0], IDX, vs_shape)
+        header = header + line
+
+    # Add IDx to file
+    if 0 < IDX:
+        header = header + "IDX = {0}\n\n".format(IDX)
+
+    # Add header to file
+    f = header + f
+
 
     print f
