f29f63e72669e964fbff7193eb5c5839566168c9
Anthony Scopatz <scopatz@gmail.com>
2010-11-22 20:26:22 -0600

    Improved tally sets to include seprent MT numbers.

--- char/tally_types.py
+++ char/tally_types.py
@@ -1,9 +1,22 @@
+# General sets
+
 total_only = {"sigma_t": -1}
 
-no_scattering = {"sigma_t": -1, "sigma_f": -2, "nubar": -3, "chi": -4, "sigma_a": -5}
+# MCNP sets
 
-basic = {"sigma_t": -1, "sigma_f": -2, "nubar": -3, "chi": -4, "sigma_a": -5, "sigma_e": 2, "sigma_i": 4}
+mcnp_no_scattering = {"sigma_t": -1, "sigma_f": -2, "nubar": -3, "chi": -4, "sigma_a": -5}
 
-advanced = {"sigma_t": -1, "sigma_f": -2, "nubar": -3, "chi": -4, "sigma_a": -5, "sigma_e": 2, "sigma_i": 4, 
+mcnp_basic = {"sigma_t": -1, "sigma_f": -2, "nubar": -3, "chi": -4, "sigma_a": -5, "sigma_e": 2, "sigma_i": 4}
+
+mcnp_advanced = {"sigma_t": -1, "sigma_f": -2, "nubar": -3, "chi": -4, "sigma_a": -5, "sigma_e": 2, "sigma_i": 4, 
+            "sigma_2n": 16, "sigma_3n": 17, "sigma_gamma": 102, "sigma_proton": 103, "sigma_alpha": 107}
+
+# Serpent Sets
+
+serpent_no_scattering = {"sigma_t": -1, "sigma_f": -6, "nubar": -7, "sigma_a": -2}
+
+serpent_basic = {"sigma_t": -1, "sigma_f": -6, "nubar": -7,  "sigma_a": -2, "sigma_e": 2, "sigma_i": 4}
+
+serpent_advanced = {"sigma_t": -1, "sigma_f": -2, "nubar": -3, "sigma_a": -2, "sigma_e": 2, "sigma_i": 4, 
             "sigma_2n": 16, "sigma_3n": 17, "sigma_gamma": 102, "sigma_proton": 103, "sigma_alpha": 107}
 
