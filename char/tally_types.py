98a539d2e823b09c9dbaf6eaa3dc9f620a4779ef
Anthony Scopatz <scopatz@gmail.com>
2010-11-22 20:50:49 -0600

    Fixed some serpent MT numbers.

--- char/tally_types.py
+++ char/tally_types.py
@@ -17,6 +17,6 @@ serpent_no_scattering = {"sigma_t": -1, "sigma_f": -6, "nubar": -7, "sigma_a": -
 
 serpent_basic = {"sigma_t": -1, "sigma_f": -6, "nubar": -7,  "sigma_a": -2, "sigma_e": 2, "sigma_i": 4}
 
-serpent_advanced = {"sigma_t": -1, "sigma_f": -2, "nubar": -3, "sigma_a": -2, "sigma_e": 2, "sigma_i": 4, 
+serpent_advanced = {"sigma_t": -1, "sigma_f": -6, "nubar": -7, "sigma_a": -2, "sigma_e": 2, "sigma_i": 4, 
             "sigma_2n": 16, "sigma_3n": 17, "sigma_gamma": 102, "sigma_proton": 103, "sigma_alpha": 107}
 
