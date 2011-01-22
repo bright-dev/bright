d1659374456c3dda849691dd3682d58bf545f2ed
Anthony Scopatz <scopatz@gmail.com>
2011-01-22 00:22:47 -0600

    Initialized a ui portion of char.

---
+++ char/ui/app.py
@@ -0,0 +1,6 @@
+"""Provides a UI application on top of char.  Currently, this is provided as a way of visualizing the
+cross-section database output.  It the future, it may also drive the char system and be able to spwan 
+and monitor runs."""
+
+from enthought.traits.api import Float, File, Str, Int, Array
+from enthought.traits.ui.api import View, Item, HGroup, VGroup
