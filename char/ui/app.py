23475fdf6fd0cd11d16ba2da83e0c9aeecd993ba
Anthony Scopatz <scopatz@gmail.com>
2011-01-23 16:25:40 -0600

    Added perturbation table into app.py

--- char/ui/app.py
+++ char/ui/app.py
@@ -3,22 +3,36 @@ cross-section database output.  It the future, it may also drive the char system
 and monitor runs."""
 
 from enthought.traits.api import HasTraits, Float, File, Str, Int, Array, Instance
-from enthought.traits.ui.api import View, Item, HGroup, VGroup
+from enthought.traits.ui.api import View, Item, HGroup, VGroup, InstanceEditor
 
 import tables as tb
+
+from char.ui.hdf5_table import Hdf5Table
 
 
 class Application(HasTraits):
     """Front-facing char application."""
 
     # Pytables Traits
-    rx_h5_path = File
+    rx_h5_path = File(filter=["H5 (*.h5)|*.h5", "HDF5 (*.hdf5)|*.hdf5", "All files|*"], auto_set=False)
     rx_h5 = Instance(tb.File)
 
-    
+    perturbations_table = Instance(Hdf5Table)
 
     traits_view = View(
-                    Item('rx_h5_path', label="Path to data:")
+                    VGroup(
+                        HGroup( Item('rx_h5_path', label="Path to data:", width=1.0) ),
+                        Item("_"),
+                        Item('perturbations_table', 
+                            editor=InstanceEditor(view='traits_view'), 
+                            style='custom', 
+                            show_label=False, 
+                            resizable=True, 
+                            ),
+                    ),
+                    width=500,
+                    height=500,
+                    resizable=True,
                   )
 
 
@@ -34,3 +48,4 @@ class Application(HasTraits):
         # Open new file
         self.rx_h5 = tb.openFile(new, 'r')
 
+        self.perturbations_table = Hdf5Table(h5=self.rx_h5, path_to_table="/perturbations")
