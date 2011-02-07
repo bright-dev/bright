1acec7b5fa3fdb07a829d17fe5e16a9525c70e72
Anthony Scopatz <scopatz@gmail.com>
2011-02-07 15:02:50 -0600

    Added MassStream class attributes

--- BriPy/src/MassStream/cpp_mass_stream.pxd
+++ BriPy/src/MassStream/cpp_mass_stream.pxd
@@ -7,8 +7,12 @@ cimport std
 
 cdef extern from "../MassStream.h":
     cdef cppclass MassStream:
+        # Constuctors
         MassStream()
         MassStream(map[int, double], float, std.string)
         MassStream(char *, float, std.string)
 
-        double mass        
+        # Attributes
+        map[int, double] comp
+        double mass
+        std.string name
--- BriPy/src/MassStream/mass_stream_wrapper.pyx
+++ BriPy/src/MassStream/mass_stream_wrapper.pyx
@@ -14,14 +14,15 @@ cdef class MassStream:
     cdef cpp_mass_stream.MassStream * ms_pointer
 
     def __cinit__(self, isovec=None, float mass=-1.0, char * name=''):
-        cdef cpp_map[int, double] cd
+        """MassStream C++ constuctor."""
+        cdef cpp_map[int, double] comp_dict
 
         if isinstance(isovec, dict):
             # Mass Stream from dict
-            cd = cpp_map[int, double]()
+            comp_dict = cpp_map[int, double]()
             for key, value in isovec.items():
-                cd[key] = value
-            self.ms_pointer = new cpp_mass_stream.MassStream(cd, mass, std.string(name))
+                comp_dict[key] = value
+            self.ms_pointer = new cpp_mass_stream.MassStream(comp_dict, mass, std.string(name))
 
         elif isinstance(isovec, basestring):
             # Mass Stream from file
@@ -37,7 +38,27 @@ cdef class MassStream:
 
 
     def __dealloc__(self):
+        """MassStream C++ destructor."""
         del self.ms_pointer
+
+
+    #
+    # Class Attributes
+    #
+
+    property comp:
+        def __get__(self):
+            comp_dict = {}
+            cdef cpp_map[int, double].iterator comp_iter = self.ms_pointer.comp.begin()
+            while comp_iter != self.ms_pointer.comp.end():
+                comp_dict[deref(comp_iter).first] = deref(comp_iter).second
+            return comp_dict
+
+        def __set__(self, dict comp):
+            cdef cpp_map[int, double] comp_dict = cpp_map[int, double]()
+            for key, value in comp.items():
+                comp_dict[key] = value
+            self.ms_pointer.comp = comp_dict
 
 
     property mass:
@@ -46,3 +67,12 @@ cdef class MassStream:
 
         def __set__(self, double mass):
             self.ms_pointer.mass = mass
+
+
+    property name:
+        def __get__(self):
+            cdef std.string ms_name = self.ms_pointer.name
+            return ms_name.c_str()
+
+        def __set__(self, char * name):
+            self.ms_pointer.name = std.string(name)
