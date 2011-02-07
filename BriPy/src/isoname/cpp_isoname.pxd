3146cc0977b4b8ec7932fb88ebc87c0006b59107
Anthony Scopatz <scopatz@gmail.com>
2011-02-06 22:30:04 -0600

    Figured out pattern for non-method function overloading.

--- BriPy/src/isoname/cpp_isoname.pxd
+++ BriPy/src/isoname/cpp_isoname.pxd
@@ -2,6 +2,33 @@
 cimport std
 
 cdef extern from "isoname.h" namespace "isoname":
+
+
+    # Current Form
+    std.string CurrentForm(int)
     std.string CurrentForm(std.string)
 
+    # LLAAAM_2_* Functions
+    int LLAAAM_2_zzaaam(std.string)
+    int LLAAAM_2_MCNP(std.string)
+
+    # zzaaam_2_* Functions
+    std.string zzaaam_2_LLAAAM(int)
     int zzaaam_2_MCNP(int)
+
+    # MCNP_2_* Functions 
+    int MCNP_2_zzaaam(int)
+    std.string MCNP_2_LLAAAM(int)
+
+    # mixed_2_*_ Functions
+    int mixed_2_zzaaam(std.string)
+    int mixed_2_zzaaam(int)
+    std.string mixed_2_LLAAAM(std.string)
+    std.string mixed_2_LLAAAM(int)
+    int mixed_2_MCNP(std.string)
+    int mixed_2_MCNP(int)
+
+    # Helper Functions
+    double nuc_weight_zzaaam(int)
+    double nuc_weight(int)
+    double nuc_weight(std.string)
--- BriPy/src/isoname/isoname_wrapper.pyx
+++ BriPy/src/isoname/isoname_wrapper.pyx
@@ -2,11 +2,31 @@
 cimport std
 cimport cpp_isoname
 
-def CurrentForm(char* name):
-    cdef std.string cpp_CurrentForm = cpp_isoname.CurrentForm(std.string(name))
+
+#def CurrentForm(char* nuc):
+#    cdef std.string cpp_CurrentForm = cpp_isoname.CurrentForm(std.string(nuc))
+#    return cpp_CurrentForm.c_str()
+    
+
+#def CurrentForm(int nuc):
+#    cdef std.string cpp_CurrentForm = cpp_isoname.CurrentForm(nuc)
+#    return cpp_CurrentForm.c_str()
+
+
+def CurrentForm(nuc):
+    """Find the current form of a nuclide."""
+    cdef std.string cpp_CurrentForm 
+
+    if isinstance(nuc, basestring):
+        cpp_CurrentForm = cpp_isoname.CurrentForm(std.string(nuc))
+    elif isinstance(nuc, int):
+        cpp_CurrentForm = cpp_isoname.CurrentForm(<int> nuc)
+    else:
+        raise TypeError("Nuclide not a string ot integer.")
+
     return cpp_CurrentForm.c_str()
+    
 
-
-def zzaaam_2_MCNP(int iso):
-    return cpp_isoname.zzaaam_2_MCNP(iso)
+def zzaaam_2_MCNP(int nuc):
+    return cpp_isoname.zzaaam_2_MCNP(nuc)
 
