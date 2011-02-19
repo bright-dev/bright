237db333fb3d063af9f28c30c26701c82d64cce2
Anthony Scopatz <scopatz@gmail.com>
2011-02-19 11:11:43 -0600

    Removed get and set functions from Reactor1G.h and changed vector<float> to vector<double>.

--- FCComps/Reactor1G.h
+++ FCComps/Reactor1G.h
@@ -28,7 +28,7 @@
 /*** Reactor1G Component Class and Functions ***/
 /***********************************************/
 
-typedef std::map<int, std::vector<float> > IsoFluenceDict;
+typedef std::map<int, std::vector<double> > IsoFluenceDict;
 typedef IsoFluenceDict::iterator IsoFluenceIter;
 
 typedef std::vector<double> Data_F_;
@@ -126,7 +126,7 @@ public:
     double VC;							//Coolant Volume
 
     std::string libfile;						//Path where the reactor's HDF5 library
-    std::vector<float> F;   					//Fluence in [n/kb]
+    std::vector<double> F;   					//Fluence in [n/kb]
     IsoFluenceDict BUi_F_;						//Burnup [MWd/kgIHM]
     IsoFluenceDict pi_F_; 						//Production rate [n/s]
         IsoFluenceDict di_F_;		      	   			//Destruction rate [n/s]
@@ -179,133 +179,6 @@ public:
     Data_F_ LatticeE_F_;						//Values for lattice function E(F)
     Data_F_ LatticeF_F_;						//Values for lattice function F(F)
 
-    //Get Functions
-    int                           get_B()                   const {return B;};
-    double                        get_phi()                 const {return phi;};
-    std::map<std::string, double> get_FuelChemicalForm()    const {return FuelChemicalForm;};
-    std::map<std::string, double> get_CoolantChemicalForm() const {return CoolantChemicalForm;};
-    double                        get_rhoF()                const {return rhoF;};
-    double                        get_rhoC()                const {return rhoC;};
-    double                        get_P_NL()                const {return P_NL;};
-    double                        get_TargetBU()            const {return TargetBU;};
-    bool                          get_useZeta()             const {return useZeta;};
-    std::string                   get_Lattice()             const {return Lattice;};
-    bool                          get_H_XS_Rescale()        const {return H_XS_Rescale;};
-
-    double                        get_r()                   const {return r;};
-    double                        get_l()                   const {return l;};
-    double                        get_S_O()                 const {return S_O;}; 
-    double                        get_S_T()                 const {return S_T;};
-    double                        get_VF()                  const {return VF;};
-    double                        get_VC()                  const {return VC;};
-
-    std::string                   get_libfile()             const {return libfile;};
-    std::vector<float>            get_F()                   const {return F;};
-    IsoFluenceDict                get_BUi_F_()              const {return BUi_F_;};
-    IsoFluenceDict                get_pi_F_()               const {return pi_F_;};
-    IsoFluenceDict                get_di_F_()               const {return di_F_;};
-    std::map<int, IsoFluenceDict> get_Tij_F_()              const {return Tij_F_;};
-
-    IsoSet get_I() const {return I;};
-    IsoSet get_J() const {return J;};
-    std::map<int, double> get_sigma_a_therm() const {return sigma_a_therm;};
-    std::map<int, double> get_sigma_s_therm() const {return sigma_s_therm;};
-
-    double                        get_A_IHM()               const {return A_IHM;};
-    double                        get_MWF()                 const {return MWF;};
-    double                        get_MWC()                 const {return MWC;};
-    CompDict                      get_niF()                 const {return niF;};
-    CompDict                      get_niC()                 const {return niC;};
-    CompDict                      get_miF()                 const {return miF;};
-    CompDict                      get_miC()                 const {return miC;};
-    CompDict                      get_NiF()                 const {return NiF;};
-    CompDict                      get_NiC()                 const {return NiC;};
-
-    Data_F_                       get_dF_F_()               const {return dF_F_;};
-    Data_F_                       get_dC_F_()               const {return dC_F_;};
-    Data_F_                       get_BU_F_()               const {return BU_F_;};
-    Data_F_                       get_P_F_()                const {return P_F_;};
-    Data_F_                       get_D_F_()                const {return D_F_;};
-    Data_F_                       get_k_F_()                const {return k_F_;};
-    std::map<int, Data_F_ >       get_Mj_F_()               const {return Mj_F_;};
-    Data_F_                       get_zeta_F_()             const {return zeta_F_;};
-
-    int                           get_fd()                  const {return fd;};
-    double                        get_Fd()                  const {return Fd;};
-    double                        get_BUd()                 const {return BUd;};
-    double                        get_k()                   const {return k;};
-
-    MassStream                    get_InU()                 const {return InU;};
-    MassStream                    get_InTRU()               const {return InTRU;};
-    MassStream                    get_InLAN()               const {return InLAN;};
-    MassStream                    get_InACT()               const {return InACT;};
-    MassStream                    get_OutU()                const {return OutU;};
-    MassStream                    get_OutTRU()              const {return OutTRU;};
-    MassStream                    get_OutLAN()              const {return OutLAN;};
-    MassStream                    get_OutACT()              const {return OutACT;};
-
-    double                        get_deltaR()              const {return deltaR;};
-    double                        get_TruCR()               const {return TruCR;};
-
-    Data_F_                       get_SigmaFa_F_()          const {return SigmaFa_F_;};
-    Data_F_                       get_SigmaFtr_F_()         const {return SigmaFtr_F_;};
-    Data_F_                       get_kappaF_F_()           const {return kappaF_F_;};
-
-    Data_F_                       get_SigmaCa_F_()          const {return SigmaCa_F_;};
-    Data_F_                       get_SigmaCtr_F_()         const {return SigmaCtr_F_;};
-    Data_F_                       get_kappaC_F_()           const {return kappaC_F_;};
-
-    Data_F_                       get_LatticeE_F_()         const {return LatticeE_F_;};
-    Data_F_                       get_LatticeF_F_()         const {return LatticeF_F_;};
-
-    //Set Functions
-    void set_B(int b)                                               {B = b;};
-    void set_phi(double p)                                          {phi = p;};
-    void set_FuelChemicalForm(std::map<std::string, double> fcf)    {FuelChemicalForm = fcf;};
-    void set_CoolantChemicalForm(std::map<std::string, double> ccf) {CoolantChemicalForm = ccf;};
-    void set_rhoF(double rF)                                        {rhoF = rF;};
-    void set_rhoC(double rC) 					{rhoC = rC;};
-    void set_P_NL(double p_nl)                                      {P_NL= p_nl;};
-    void set_TargetBU(double tbu)                                   {TargetBU = tbu;};
-    void set_useZeta(bool uZ)                                       {useZeta = uZ;}; 	
-    void set_Lattice(std::string lt)                                {Lattice = lt;};
-    void set_H_XS_Rescale(bool hxs)                                 {H_XS_Rescale = hxs;}; 	
-
-    void set_r(double R)                                            {r = R;};
-    void set_l(double L)                                            {l = L;};
-    void set_S_O(double s_o)                                        {S_O = s_o;};
-    void set_S_T(double s_t)                                        {S_T = s_t;};
-    void set_VF(double vf)                                          {VF = vf;};
-    void set_VC(double vc)                                          {VC = vc;};
-
-    void set_libfile(std::string lf)                                {libfile = lf;};
-
-    void set_A_IHM(double a_ihm)                                    {A_IHM = a_ihm;};
-    void set_MWF(double mwf)                                        {MWF = mwf;};
-    void set_MWC(double mwc)                                        {MWF = mwc;};
-    void set_niF(CompDict NIf)                                      {niF = NIf;};
-    void set_niC(CompDict NIc)                                      {niC = NIc;};
-    void set_miF(CompDict MIf)                                      {miF = MIf;};
-    void set_miC(CompDict MIc)                                      {miC = MIc;};
-    void set_NiF(CompDict nIf)                                      {NiF = nIf;};
-    void set_NiC(CompDict nIc)                                      {NiC = nIc;};
-
-    void set_fd(int FD)                                             {fd = FD;};
-    void set_Fd(double fD)                                          {Fd = fD;};
-    void set_BUd(double buD)                                        {BUd = buD;};
-    void set_k(double K)                                            {k = K;};
-
-    void set_InU(MassStream ms)                                     {InU = ms;};
-    void set_InTRU(MassStream ms)                                   {InTRU = ms;};
-    void set_InLAN(MassStream ms)                                   {InLAN = ms;};
-    void set_InACT(MassStream ms)                                   {InACT = ms;};
-    void set_OutU(MassStream ms)                                    {OutU = ms;};
-    void set_OutTRU(MassStream ms)                                  {OutTRU = ms;};
-    void set_OutLAN(MassStream ms)                                  {OutLAN = ms;};
-    void set_OutACT(MassStream ms)                                  {OutACT = ms;};
-
-    void set_deltaR(double dR)                                      {deltaR = dR;};
-    void set_TruCR(double tcr)                                      {TruCR = tcr;};
 
     //Public access functions
     void         initialize(ReactorParameters);
