8eca7aa3b5170ba40fe0f7a3f7fee84d6b5ac262
Anthony Scopatz <scopatz@gmail.com>
2011-04-08 20:36:17 -0500

    Converted env to dict interface in envchar.

--- char/envchar.py
+++ char/envchar.py
@@ -193,142 +193,143 @@ class RemoteConnection(object):
 
 
 
-def update_env_for_execution(env):
+def update_env_for_execution(env[:
     """Updates the env namespace for runs where an execution is going to occur."""
     # Make isotopic lists
-    if isinstance(env.core_load_isos, basestring):
-        env.core_load = iso_file_conversions(env.core_load_isos)
-    elif isinstance(env.core_load_isos, list):
-        env.core_load = iso_list_conversions(env.core_load_isos)
+    if isinstance(env['core_load_isos'], basestring):
+        env['core_load'] = iso_file_conversions(env['core_load_isos'])
+    elif isinstance(env['core_load_isos'], list):
+        env['core_load'] = iso_list_conversions(env['core_load_isos'])
     else:
         raise TypeError("The core_load_isos type was not correct.")
 
-    if isinstance(env.core_transmute_isos, basestring):
-        env.core_transmute = iso_file_conversions(env.core_transmute_isos)
-    elif isinstance(env.core_transmute_isos, list):
-        env.core_transmute = iso_list_conversions(env.core_transmute_isos)
+    if isinstance(env['core_transmute_isos'], basestring):
+        env['core_transmute'] = iso_file_conversions(env['core_transmute_isos'])
+    elif isinstance(env['core_transmute_isos, list):
+        env['core_transmute'] = iso_list_conversions(env['core_transmute_isos'])
     else:
         raise TypeError("The core_transmute_isos type was not correct.")
 
     # Find which isotopes are available in serpent
     # and which ones must be handled manually.
-    core_transmute_set = set(env.core_transmute['zzaaam'])
-    serpent_xs_isos_set = serpent_xs_isos_available(env.serpent_xsdata)
+    core_transmute_set = set(env['core_transmute']['zzaaam'])
+    serpent_xs_isos_set = serpent_xs_isos_available(env['serpent_xsdata'])
 
     core_transmute_in_serpent = list(core_transmute_set & serpent_xs_isos_set)
     core_transmute_not_in_serpent = list(core_transmute_set - serpent_xs_isos_set)
 
-    env.core_transmute_in_serpent = iso_list_conversions(core_transmute_in_serpent)
-    env.core_transmute_not_in_serpent = iso_list_conversions(core_transmute_not_in_serpent)
+    env['core_transmute_in_serpent'] = iso_list_conversions(core_transmute_in_serpent)
+    env['core_transmute_not_in_serpent'] = iso_list_conversions(core_transmute_not_in_serpent)
 
     # Make temperature flag
-    if not hasattr(env, 'temperature'):
-        env.temperature = 600
+    if 'temperature' not in env:
+        env['temperature'] = 600
 
-    env.temp_flag = temperature_flag(env.temperature)
+    env['temp_flag'] = temperature_flag(env['temperature'])
 
     # Grab the MT numbers that are available for valid isotopes.
-    env.iso_mts = serpent_mt_avaliable(env.serpent_xsdata, 
-                                           env.core_transmute_in_serpent['zzaaam'], 
-                                           env.temp_flag, 
-                                           env.verbosity)
+    env['iso_mts'] = serpent_mt_avaliable(env['serpent_xsdata'], 
+                                          env['core_transmute_in_serpent']['zzaaam'], 
+                                          env['temp_flag'], 
+                                          env['verbosity'])
 
     # Make fuel stream
-    env.IHM_stream = MassStream(env.initial_heavy_metal)
+    env['IHM_stream'] = MassStream(env['initial_heavy_metal'])
 
-    if hasattr(env, 'sensitivity_mass_fractions'):
-        env.deltam = np.atleast_1d(env.sensitivity_mass_fractions)
-        env.deltam.sort()
+    if 'sensitivity_mass_fractions' in env:
+        env['deltam'] = np.atleast_1d(env['sensitivity_mass_fractions'])
+        env['deltam'].sort()
 
 
     # Make arrays out of quatities that are allowed to vary.
-    env.fuel_density = np.atleast_1d(env.fuel_density)
-    env.clad_density = np.atleast_1d(env.clad_density)
-    env.cool_density = np.atleast_1d(env.cool_density)
+    env['fuel_density'] = np.atleast_1d(env['fuel_density'])
+    env['clad_density'] = np.atleast_1d(env['clad_density'])
+    env['cool_density'] = np.atleast_1d(env['cool_density'])
     
-    env.fuel_cell_radius = np.atleast_1d(env.fuel_cell_radius)
-    env.void_cell_radius = np.atleast_1d(env.void_cell_radius)
-    env.clad_cell_radius = np.atleast_1d(env.clad_cell_radius)
+    env['fuel_cell_radius'] = np.atleast_1d(env['fuel_cell_radius'])
+    env['void_cell_radius'] = np.atleast_1d(env['void_cell_radius'])
+    env['clad_cell_radius'] = np.atleast_1d(env['clad_cell_radius'])
 
-    env.unit_cell_pitch = np.atleast_1d(env.unit_cell_pitch)
+    env['unit_cell_pitch'] = np.atleast_1d(env['unit_cell_pitch'])
 
-    env.burn_regions = np.atleast_1d(env.burn_regions)
-    env.fuel_specific_power = np.atleast_1d(env.fuel_specific_power)
+    env['burn_regions'] = np.atleast_1d(env['burn_regions'])
+    env['fuel_specific_power'] = np.atleast_1d(env['fuel_specific_power'])
 
     # Grab initial iso perturbation
     max_mass = 0.0
-    initial_iso_vars = []
-    for var in env.__dict__:
-        m = re.match(initial_iso_pattern, var)
+    initial_iso_keys = []
+    for key in env:
+        m = re.match(initial_iso_pattern, key)
         if m is None:
             continue
 
-        env_initial_iso = getattr(env, var)
+        env_initial_iso = env[key]
         env_initial_iso = np.atleast_1d(env_initial_iso)
 
-        initial_iso_vars.append(var)
+        initial_iso_keys.append(key)
         max_mass += np.max(env_initial_iso)
 
-    initial_iso_vars.sort()
-    env.initial_iso_vars = initial_iso_vars
+    initial_iso_keys.sort()
+    env['initial_iso_keys'] = initial_iso_keys
 
     if 1.0 < max_mass:
         print(failure("The maxium mass of initial heavy metal perturbations exceeds 1.0 kg!"))
         raise SystemExit
 
     # Set up tuple of parameters to perform a burnup step for
-    env.perturbation_params = ['fuel_density', 
-                                'clad_density', 
-                                'cool_density',
+    env['perturbation_params'] = ['fuel_density', 
+                                  'clad_density', 
+                                  'cool_density',
 
-                                'fuel_cell_radius', 
-                                'void_cell_radius', 
-                                'clad_cell_radius', 
+                                  'fuel_cell_radius', 
+                                  'void_cell_radius', 
+                                  'clad_cell_radius', 
 
-                                'unit_cell_pitch', 
-                                'burn_regions', 
-                                'fuel_specific_power',]
+                                  'unit_cell_pitch', 
+                                  'burn_regions', 
+                                  'fuel_specific_power',]
 
-    env.perturbation_params.extend(initial_iso_vars)
+    env['perturbation_params'].extend(initial_iso_keys)
  
-    env.perturbation_params.append('burn_times')   # burn_times needs to be the last element
+    # burn_times needs to be the last element
+    env['perturbation_params'].append('burn_times')   
 
     return env
 
 
 def update_env(env):
-    """Takes the env namespace, updates it, and returns it."""
-    if hasattr(env, 'tallies'):
-        env.tallies_reversed = metasci.ReverseDic(env.tallies)
+    """Takes the env[' namespace, updates it, and returns it."""
+    if 'tallies' in env:
+        env['tallies_reversed'] = metasci.ReverseDic(env['tallies'])
 
-    if not hasattr(env, 'scheduler'):
-        env.scheduler = ''
+    if 'scheduler' not in env:
+        env['scheduler'] = ''
 
     # Name some files and directories
-    env.input_file = env.reactor + ".i"
-    env.run_script = 'run_{0}.sh'.format(env.reactor)
+    env['input_file'] = env['reactor'] + ".i"
+    env['run_script'] = 'run_{0}.sh'.format(env['reactor'])
 
-    if hasattr(env, 'remote_dir'):
-        env.remote_dir = env.remote_dir + "runchar/{0}/".format(env.reactor)
+    if 'remote_dir' in env:
+        env['remote_dir'] = env['remote_dir'] + "runchar/{0}/".format(env['reactor'])
 
     # Setup a remote connection instance
     rckw = {}
-    if hasattr(env, 'remote_url'):
-        rckw['url'] = env.remote_url
-    if hasattr(env, 'remote_user'):
-        rckw['user'] = env.remote_user
-    if hasattr(env, 'remote_dir'):
-        rckw['dir'] = env.remote_dir
-    env.remote_connection = RemoteConnection(**rckw)
+    if 'remote_url' in env:
+        rckw['url'] = env['remote_url']
+    if 'remote_user' in env:
+        rckw['user'] = env['remote_user']
+    if 'remote_dir' in env:
+        rckw['dir'] = env['remote_dir']
+    env['remote_connection'] = RemoteConnection(**rckw)
 
     # Make Time Steps 
-    env.burn_times = np.arange(0, env.burn_time + env.time_step/10.0, env.time_step)
-    env.burn_times_index = range(len(env.burn_times))
+    env['burn_times'] = np.arange(0, env['burn_time'] + env['time_step']/10.0, env['time_step'])
+    env['burn_times_index'] = range(len(env['burn_times']))
 
     # Upddate the namespace if we are going to execute a neutronics code
-    if (env.options.MAKE_INPUT or env.options.RUN_TRANSPORT or 
-        env.options.RUN_BURNUP or env.options.RUN_XS_GEN or 
-        env.options.RUN_DELTAM):
+    if (env['options'].MAKE_INPUT or env['options'].RUN_TRANSPORT or 
+        env['options'].RUN_BURNUP or env['options'].RUN_XS_GEN or 
+        env['options'].RUN_DELTAM):
 
         env = update_env_for_execution(env)
 
