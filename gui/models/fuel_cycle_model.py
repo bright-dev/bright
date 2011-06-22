ee10c05268491a2d123f52e2ce212303df2c85c9
Kevin Lam <simiopolis@gmail.com>
2011-06-22 18:51:05 -0500

    Modified class registration in fuel_cycle_model

--- gui/models/fuel_cycle_model.py
+++ gui/models/fuel_cycle_model.py
@@ -1,17 +1,18 @@
 from traits.api import HasTraits, Str, Dict
+from class_models.class_model import ClassModel
+
 
 import os
-
+import re
 
 class FuelCycleModel(HasTraits):
 
     def __init__(self, *args, **kwargs):
         super(FuelCycleModel, self).__init__(*args, **kwargs)
         self.register_classes_available()
-        
+       
 
     script = Str
-
     variables = Dict
     classes_available = Dict
     classes_imported = Dict
@@ -22,18 +23,29 @@ class FuelCycleModel(HasTraits):
 
 
     def add_instance(self, class_name):
-        #        
+        pass       
 
     def register_classes_available(self):
-        #os, exec
-        list = os.listdir('class_modles')
+        '''\os, exec
+        dirlist = os.listdir('class_models')
         
-        for i in list:
-            if "model" in i:
-                classes_available[i.split('_models.py')[0]] = i
+        for i in dirlist:
+            if "model.py" in i:
+                self.classes_available[i.split('_model.py')[0]] = i'''
+        localdict = {}
+        
+        dirlist = os.listdir('class_models')
+        
+        for i in dirlist:
+            match = re.search('(.+_model).py$', i)
+            if match:
+                exec('from class_models.{0} import *'.format(match.group(1)), {}, localdict)
+                for key, value in localdict.items():
+                    if issubclass(value, ClassModel):
+                       self.classes_available[key] = value 
 
 if __name__ == "__main__":
     fcm = FuelCycleModel()
-    fcm.add_instance("Storage")
-
-    print fcm.script
+   # fcm.add_instance("Storage")
+   
+    print fcm.classes_available
