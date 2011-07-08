73bcbd8fefdaa4e58dcd958c3763e7d4a2f78ac1
Kevin Lam <simiopolis@gmail.com>
2011-07-08 16:26:28 -0500

    Added a follow_path moethod to parse through nodes.

--- gui/models/fuel_cycle_model.py
+++ gui/models/fuel_cycle_model.py
@@ -47,8 +47,12 @@ class FuelCycleModel(HasTraits):
         var = self.classes_available[class_name](var=varname, extra_data_parameter = data_dict) #class definition is stored in var
         self.classes_imported.add(class_name) #add the name of class selected to a set
         self.variables[varname] = var #store in dictionary with varname as key and var as value
-        
+
         self.add_node(var)
+        
+        '''\if self.graph.number_of_nodes() == 1:
+            first_node = self.graph.nodes()[0]
+            print first_node'''
           
     def register_classes_available(self):
         """Check the class_models directory for all available models and record them into the classes_available dictionary."""
@@ -78,17 +82,23 @@ class FuelCycleModel(HasTraits):
            an attribute called msname in the edge.
            Format:
            add_edge([node1], [node2], [optional mass stream name]"""
-                   
+               
         #calculate the walk_value for edge
-        temp_list = [self.graph.get_edge_data(j,k)['walk_value'] for j,k in self.graph.edges()]
-        if len(temp_list) == 0:
+        #temp_list = [self.graph.get_edge_data(j,k)['walk_value'] for j,k in self.graph.edges
+        if self.graph.out_degree(node1) > 0:
+            temp_list = [self.graph[j][k]['walk_value'] for j,k in self.graph.out_edges(node1)]            
+            walk_value = max(temp_list) + 1
+        else:
+            walk_value = 1
+    
+        
+        '''\if len(temp_list) == 0:
             walk_value = 1
         else:
-            walk_value = max(temp_list) + 1
-
+            walk_value = max(temp_list) + 1'''
+    
         #create edge with calculated walk_value, walked flag, and msname        
         self.graph.add_edge(node1, node2, walk_value=walk_value, walked=False, msname=ms_value)
-        
         self.convert_to_script()
 
     def calc_comp(self, varname, msname, ms_value = None):
@@ -101,9 +111,9 @@ class FuelCycleModel(HasTraits):
         
         #self.graph.add_edge(self.variables[varname], self.variables[msname])    
         if ms_value is not None:
-            self.add_edge(self.variables[varname], self.variables[msname], ms_value)
+            self.add_edge(self.variables[msname], self.variables[varname], ms_value)
         else:
-            self.add_edge(self.variables[varname], self.variables[msname])        
+            self.add_edge(self.variables[msname], self.variables[varname])        
     
     def remove_variable(self, varname):
         """Deletes everything related to a specified variable from memory.
@@ -154,20 +164,89 @@ class FuelCycleModel(HasTraits):
                 
         #self.script_execution = self.script_execution + self.variables[varname].add_calc(msname) + '\n'
         #execution lines
-        temp_script3 = ""
+        '''/temp_script3 = ""
         for key, value in self.graph.edges():
             temp_script3 = temp_script3 + key.add_calc(value.var) + '\n'
-        self.script_execution = temp_script3   
+        self.script_execution = temp_script3'''
 
+        '''\ temp_list = []
+        for j,k in self.graph.edges():
+            temp_list.append(self.graph[j][k]['walk_value'])
+            temp_list.sort()
+            for i in temp_list:
+                for j,k in self.graph.edges():
+                    if self.graph[j][k]['walk_value'] == i:'''
+        nodes = self.graph.nodes()
+        pred = {node:self.graph.predecessors(node) for node in nodes}       
+        pred_len = {node:len(pred[node]) for node in nodes}
+        
+        success = {node:self.graph.successors(node) for node in nodes}
+        success_len = {node:len(success[node]) for node in nodes}
+        
+        for node in nodes:
+            if (pred_len[node] == 0) and (success_len[node] > 0):
+                self.follow_path(node)
+                
+    
+    def follow_path(self, node):
+        temp_script3 = ""    
+        var = 1
+        original_node = node
+        cycle_element_used = set()
+        while var == 1:
+            edges = self.graph.out_edges(node)
+            for j,k in edges:
+                if self.graph.edge[j][k]['walked'] == True:
+                    edges.remove((j,k))
+                if len(nx.simple_cycles(self.graph)) > 0:
+                   cycles_list = nx.simple_cycles(self.graph)[0]
+                   if j in cycles_list and not cycle_element_used:
+                        temp_script3 = temp_script3 + "for n in range(10):\n"
+                        cycles_list.pop()
+                        for i in range(len(cycles_list)):
+                            temp_script3 = temp_script3 + "    " + cycles_list[i].add_calc(cycles_list[i-1].var, self.graph.edge[cycles_list[i-1]][cycles_list[i]]['msname']) + '\n'                             
+                        cycle_element_used.add(j)
+                        cycle_element_used.add(k)
+            walk_list = {self.graph.edge[j][k]['walk_value']:(j,k) for j,k in edges}
+            if len(walk_list) > 0:
+                x,y =  walk_list[min(walk_list)]
+                self.graph.edge[x][y]['walked'] = True
+                if y.add_calc(x.var, self.graph.edge[x][y]['msname']) not in temp_script3:
+                    temp_script3 = temp_script3 + y.add_calc(x.var, self.graph.edge[x][y]['msname']) + '\n'
+                
+                if len(self.graph.successors(node)) > 1:
+                    node = y
+                else:
+                    node = self.graph.successors(node)[0]
+            else:
+                #test = [self.graph.edge[j][k]['walked'] for j,k in original_node]
+                #print test
+                for j,k in self.graph.out_edges():
+                    self.graph.edge[j][k]['walked'] = False
+                break
+        self.script_execution = temp_script3
+        
+
+
+
+
+    
+    
 if __name__ == "__main__":
     fcm = FuelCycleModel()
-    fcm.add_instance("sr1","Storage")
     fcm.add_instance("sr2","Storage")
-    #fcm.add_instance("enr1", "Enrichment")
+    fcm.add_instance("sr1","Storage")
+    fcm.add_instance("sr3","Storage")
+    fcm.add_instance("sr4","Storage")
+    fcm.add_instance("sr5","Storage")
+    fcm.add_instance("sr6","Storage")
     fcm.add_instance("ms1","MassStream",{922350:1.0})
-    fcm.calc_comp("sr1","ms1", "ms_prod")
-    fcm.calc_comp("sr2","ms1", "ms_prod")
-    fcm.calc_comp("sr1","sr2")
+    fcm.calc_comp("sr1","ms1")
+    fcm.calc_comp("sr5","ms1")
+    fcm.calc_comp("sr2","sr1", "ms_prod")
+    fcm.calc_comp("sr3","sr2", "ms_tail")
+    fcm.calc_comp("sr4","sr3", "ms_prod")
+    fcm.calc_comp("sr2","sr4", "ms_prod23")
     #fcm.configure_bright(write_text = False, write_hdf5 = True)  
     #fcm.configure_bright(track_isos = set([10010, 80160, 922380]))
     #fcm.remove_variable("sr1")
