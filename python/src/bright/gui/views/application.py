74128dfea954790457b6a05ce5d9bced43bd30fc
Anthony Scopatz <scopatz@gmail.com>
2011-07-08 19:28:12 -0500

    some print statements.

--- python/src/bright/gui/views/application.py
+++ python/src/bright/gui/views/application.py
@@ -14,14 +14,19 @@ class Application(HasTraits):
     
     def _model_default(self):
         fcm = FuelCycleModel()
-        fcm.add_instance("nu", "MassStream", {922380:0.992745, 922350:0.0072, 922340:0.000055})
+        fcm.add_instance("nu", "MassStream", {922380: 0.992745, 922350: 0.0072, 922340: 0.000055})
         fcm.add_instance("sr1", "Storage")
         #fcm.calc_comp("sr1","ms1")
+        print "Model = ", fcm
+        print "Graph = ", fcm.graph
+        print 
         return fcm
     
     def _graph_view_default(self):
-        m = self.model.graph
-        return GraphView(graph = self.model.graph)
+        print "Model = ", self.model
+        print "Graph = ", self.model.graph
+        print 
+        return GraphView(graph=self.model.graph, layout='tree')
     
     @on_trait_change('model.graph')
     def update_graph_view(self):
