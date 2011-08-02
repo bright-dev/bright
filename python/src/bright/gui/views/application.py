f247df99fec85316230a64751f4864b94b8b21f2
Kevin Lam <simiopolis@gmail.com>
2011-08-02 17:36:58 -0500

    Added an open and save button (save is not functional at the moment), classes availabel and variables table.

--- python/src/bright/gui/views/application.py
+++ python/src/bright/gui/views/application.py
@@ -1,5 +1,6 @@
-from traits.api import HasTraits, Instance, on_trait_change, DelegatesTo, Dict
-from traitsui.api import View, InstanceEditor, Item, HGroup, VGroup, Tabbed, CodeEditor, ShellEditor
+from traits.api import HasTraits, Instance, on_trait_change, DelegatesTo, Dict, List, Set, Str, File, Button
+from traitsui.api import View, InstanceEditor, Item, HGroup, VGroup, Tabbed, CodeEditor, ShellEditor, FileEditor, SetEditor, TableEditor, ListEditor
+from traitsui.file_dialog import open_file
 from enable.api import ComponentEditor
 from bright.gui.models.fuel_cycle_model import FuelCycleModel
 from graphcanvas.api import GraphView
@@ -12,16 +13,32 @@ class Application(HasTraits):
     script = DelegatesTo('model')
     model_context = Dict
     #graph_changed_event = DelegatesTo('model')
+    #classes_available = Dict
+    classes_list = List    
+    blah = List
+    file_name = File
+    open = Button('Open')
+    save = Button('Save')
 
     traits_view = View(
                     VGroup(
                         HGroup(
+                            Item('open', show_label = False, width = .05),
+                            Item('save', show_label = False, width = .05),
+                            Item('file_name', show_label = False, width = .05)
+                            
+                              ),
+                        HGroup(
+                            
+                            Item('classes_list', editor = ListEditor(), show_label = False, resizable = True, width =.25),
                             Item('_container', editor = ComponentEditor(), show_label = False, resizable = True, width =.25),
                             Item('script', editor = CodeEditor(), show_label = False, resizable = True, width = .50)
                             ),
-                    
-                        Item('model_context', editor = ShellEditor(share = True), label = 'Shell')
-                        ),
+                        HGroup(
+                            Item('blah', editor = ListEditor(), show_label = False, resizable = True, width =.15),
+                            Item('model_context', editor = ShellEditor(share = True), label = 'Shell')
+                              )
+                          ),
                   resizable = True
                     )
     
@@ -46,6 +63,31 @@ class Application(HasTraits):
     def _model_context_default(self):
         return {'fc': self.model}
 
+    def _classes_list_default(self):
+        fcm = FuelCycleModel()
+        list_temp = []
+        for key, value in fcm.classes_available.items():
+            list_temp.append(key)
+        return list_temp
+
+    def _blah_default(self):
+        temp_list = []    
+        for key, value in self.model.variables.items():
+            temp_list.append(key)
+        return temp_list
+
+    def _script_changed(self):
+        temp_list = []
+        for key, value in self.model.variables.items():
+            temp_list.append(key)
+        self.blah = temp_list
+
+    def _open_changed(self):
+        file_name = open_file()
+        if file_name != '':
+            self.file_name = file_name
+
+    
 if __name__ == '__main__':
     app = Application()
     app.configure_traits()
