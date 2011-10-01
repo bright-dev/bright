c26f889c7737f74aa6b69d71aefbd58cc82a775c
Anthony Scopatz <ascopatz@enthought.com>
2011-10-01 09:46:33 -0700

    Partial setup.py refactor.

--- setup.py
+++ setup.py
@@ -15,32 +15,98 @@ import numpy as np
 
 from setup_data import INFO
 
-# Clean up old files
-cython_cpp_files = ['src/stlconverters.cpp', 
-                    'src/isoname/isoname.cpp', 
-                    'src/mass_stream/mass_stream.cpp', 
-                    'src/bright/bright.cpp', 
-                    ]
-
-for f in cython_cpp_files:
-    if os.path.exists(f):
-        print "Removing {0}...".format(f)
-        os.remove(f)
 
 
 
 ###########################################
 ### Set compiler options for extensions ###
 ###########################################
-src_dir = os.path.abspath(os.path.join('src', ''))
-cpp_dir = os.path.abspath(os.path.join('..', 'cpp'))
-dat_dir = os.path.abspath(os.path.join('..', 'data'))
+brt_dir = os.path.join('bright')
+cpp_dir = os.path.join('cpp')
+dat_dir = os.path.join('data')
 
 # Get numpy include dir
 numpy_include = np.get_include()
 
+# Get PyNE include and lib dirs
+from pyne import pyne_config
+pyne_lib = pyne_config.lib
+pyne_include = pyne_config.includes
+
 # Path to user's home directory
-user_home = os.path.expanduser('~')
+#user_home = os.path.expanduser('~')
+
+# HDF5 stuff
+posix_hdf5_libs = ["z", "m", "hdf5", "hdf5_hl", "hdf5_cpp", "hdf5_hl_cpp",]
+nt_hdf5_libs = ["/DEFAULTLIB:szip.lib", "/DEFAULTLIB:zlib1.lib", "/DEFAULTLIB:hdf5dll.lib",
+                "/DEFAULTLIB:hdf5_hldll.lib", "/DEFAULTLIB:hdf5_cppdll.lib", "/DEFAULTLIB:hdf5_hl_cppdll.lib", ]
+nt_hdf5_extra_compile_args = ["/EHsc"]
+nt_hdf5_macros = [("_WIN32", None), ("_HDF5USEDLL_", None), ("HDF5CPP_USEDLL", None), ]
+
+
+
+def cpp_ext(name, sources, libs=None, use_hdf5=False):
+    """Helper function for setting up extension dictionary.
+
+    Parameters
+    ----------
+    name : str
+        Module name
+    sources : list of str
+        Files to compile
+    libs : list of str
+        Additional files to link against
+    use_hdf5 : bool
+        Link against hdf5?
+    """
+    ext = {'name': name}
+
+    ext['sources'] = [os.path.join(cpp_dir, s) for s in sources if s.endswith('cpp')] + \
+                     [os.path.join(brt_dir, s) for s in sources if s.endswith('pyx')] + \
+                     [s for s in sources if not any([s.endswith(suf) for suf in ['cpp', 'pyx']])]
+
+    ext["libraries"] = []
+    ext['include_dirs'] = [brt_dir, cpp_dir, pyne_include, numpy_include]
+    ext['language'] = "c++"
+
+    # may need to be more general
+    ext['library_dirs'] = ['build/lib/bright/lib',
+                           'build/lib.{0}-{1}/bright/lib'.format(get_platform(), get_python_version()),
+                           ]
+    # perfectly general, thanks to dynamic runtime linking of $ORIGIN
+    #ext['runtime_library_dirs'] = ['${ORIGIN}/lib', '${ORIGIN}']
+    ext['runtime_library_dirs'] = ['${ORIGIN}/lib', '${ORIGIN}', '${ORIGIN}/.']
+    #ext['runtime_library_dirs'] = ['${ORIGIN}/lib', '${ORIGIN}', '${ORIGIN}/.'] + \
+    #                              [os.path.abspath(p) for p in ext['library_dirs']] + \
+    #                              [os.path.abspath(p + '/pyne/lib') for p in sys.path] + \
+    #                              [os.path.abspath(p + '/pyne') for p in sys.path] + \
+    #                              [os.path.abspath(p) for p in sys.path]
+
+
+    if os.name == 'posix':
+        #ext["extra_compile_args"] = ["-Wno-strict-prototypes"]
+        ext["undef_macros"] = ["NDEBUG"]
+        if use_hdf5:
+            ext["libraries"] += posix_hdf5_libs
+        if libs is not None:
+            ext["libraries"] += libs
+    elif os.name == 'nt':
+        ext["extra_compile_args"] = ["/EHsc"]
+        ext["define_macros"] = [("_WIN32", None)]
+
+        if use_hdf5:
+            ext["libraries"] += nt_hdf5_libs
+            ext["extra_compile_args"] += nt_hdf5_extra_compile_args
+            ext["define_macros"] += nt_hdf5_macros
+
+        if libs is not None:
+            ext["libraries"] += libs
+
+    return ext
+
+
+
+
 
 
 
