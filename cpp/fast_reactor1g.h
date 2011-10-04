f79673ac538f796d9d6ad814acd3a5527050d2fb
Anthony Scopatz <ascopatz@enthought.com>
2011-10-03 18:06:14 -0700

    Updated fast r1g

---
+++ cpp/fast_reactor1g.h
@@ -0,0 +1,37 @@
+// Header for One-Group Fast Reactor Model
+
+#if !defined(_BRIGHT_FR_1G_)
+#define _BRIGHT_FR_1G_
+
+//Bright Libs
+#include "reactor1g.h"
+
+namespace bright {
+
+  static std::string fr_p2t [] = {"BUd", "TRUCR", "P_NL", "U", "TRU", "ACT", "LAN", "FP"};
+  static std::set<std::string> fr_p2track (fr_p2t, fr_p2t+8);
+
+  /*****************************************************/
+  /*** Fast Reactor 1G Component Class and Functions ***/
+  /*****************************************************/
+
+  class FastReactor1G : public Reactor1G
+  {
+  /** One-Group Fast Reactor Model Class.
+  *  Has default, but overridable values, for a fast reactor parameters that are loaded on initiation.
+  */
+  public:
+    // FastReactor1G Constructors
+    FastReactor1G();
+    FastReactor1G(std::string, std::string = "");
+    FastReactor1G(ReactorParameters, std::string = "");
+    FastReactor1G(std::string, ReactorParameters, std::string = "");
+    ~FastReactor1G();
+
+    //Public access functions
+    void calc_params();
+  };
+
+// end bright
+};
+#endif
