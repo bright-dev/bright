f75c2eff1d4f955b6acbafe30b2af20a9cc624b9
Anthony Scopatz <ascopatz@enthought.com>
2011-10-27 13:29:45 -0500

    Added ui and run subpackages to setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ setup(name="CHAR",
 	author='Anthony Scopatz',
 	author_email='scopatz@gmail.com',
 	url='http://www.scopatz.com/',
-	packages=['char', 'char.templates', 'char.templates.lwr'],
+	packages=['char', 'char.templates', 'char.templates.lwr'
+              'char.ui', 'char.run'],
 	package_dir={'char': 'char'}, 
     scripts=['char/scripts/char'],
 	)
