dd346229e26609a2f7047d57cbbcc9d486f19613
Siwan Kim <siwan.kim@utexas.edu>
2012-03-06 15:06:08 -0600

    march3rd

---
+++ bright/gui/views/custom_graph_canvas/io_coordinate.py
@@ -0,0 +1,15 @@
+class IOPair:
+    coordinate = [0,0]
+    left = coordinate[0]
+    right = coordinate[1]
+
+    def set_coordinate(self,x,y):
+        self.coordinate = [x,y]
+	self.left = self.coordinate[0]
+	self.right = self.coordinate[1]
+
+"""
+pair = io_pair()
+pair.set_coordinate(1,2)
+print pair.coordinate, pair.left, pair.right
+"""
