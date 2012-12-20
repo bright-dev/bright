83676eb78597ad397a337d6f39c2b2a8e1f33998
Anthony Scopatz <scopatz@gmail.com>
2012-12-19 21:06:11 -0600

    First cut at type canonical form.

--- bright/apigen/typesystem.py
+++ bright/apigen/typesystem.py
@@ -1,16 +1,96 @@
 """Implements a simple type system for API generation."""
+from collections import Sequence
 
+BASE_TYPES = set(['char', 'str', 'int32', 'int64', 'uint32', 'uint64', 'float32', 
+                  'float64', 'complex128', 'void', 'bool'])
+
+type_aliases = {
+    'i': 'int32',
+    'i4': 'int32',
+    'i8': 'int64',
+    'int': 'int32',
+    'ui': 'uint32',
+    'ui4': 'uint32',
+    'ui8': 'uint64',
+    'uint': 'uint32',
+    'f': 'float64',
+    'f4': 'float32',
+    'f8': 'float64',
+    'float': 'float64',
+    'complex': 'complex128',
+    'b': 'bool',
+    'v': 'void',
+    's': 'str',
+    'string': 'str',
+    # 'c' has char / complex ambiquity, not included
+    }
+
+
+# template types are types whose instantiations are based on meta-types
+# this dict maps their names to meta-type names in order.
+template_types = {
+    'map': ('key_type', 'value_type'),
+    'dict': ('key_type', 'value_type'),
+    'set': ('value_type',),
+    'vector': ('value_type',),
+    }
+
+
+# This is a mapping from refinement type names to the parent types.
+# The parent types may either be base types, compound types, template 
+# types, or other refined types!
+refined_types = {
+    'nucid': 'int32',
+    'nucname': 'str',
+    }
+
+def _raise_type_error(t):
+    raise TypeError("type of {0!r} could not be determined".format(t))
 
 def canon(t):
-
+    """Turns the type into a canonical form."""
+    if isinstance(t, basestring):
+        if t in BASE_TYPES:
+            return t
+        elif t in type_aliases:
+            return type_aliases[t]
+        elif t in refined_types:
+            return (canon(refined_types[t]), t)
+        else:
+            #parse_type(t)
+            _raise_type_error(t)
+    elif isinstance(t, Sequence):
+        tlen = len(t)
+        if 0 == tlen:
+            _raise_type_error(t)
+        last_val = 0 if tlen == 1 else t[-1]
+        if isinstance(t[0], basestring):
+            if t[0] in template_types:
+                templen = len(template_types[t[0]])
+                last_val = 0 if tlen == 1 + templen else t[-1]
+                filledt = [t[0]] + [canon(tt) for tt in t[1:1+templen]] + [last_val]
+                return tuple(filledt)
+            else:
+                if 2 < tlen:
+                    _raise_type_error(t)
+                return (canon(t[0]), last_val)
+        elif isinstance(t[0], Sequence):
+            if 2 < tlen:
+                _raise_type_error(t)
+            return (tuple([canon(subt) for subt in t[0]]), last_val)
+        else:
+            _raise_type_error(t)
+    else:
+        _raise_type_error(t)
 
 _cython_c_base_types = {
+    'char': 'char',
     'str': 'std_string',
-    'int': 'int',
-    'uint': 'extra_types.uint',  # 'unsigned int'
-    'float': 'float',
-    'double': 'double',
-    'complex': 'extra_types.complex_t',
+    'int32': 'int',
+    'uint32': 'extra_types.uint',  # 'unsigned int'
+    'float32': 'float',
+    'float64': 'double',
+    'complex128': 'extra_types.complex_t',
     }
 
 
